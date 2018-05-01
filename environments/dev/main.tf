787a59c3b6cd5fece854dc2d978608d9746c926a
Chris Mason <chris@MASONDESKTOP.mason.ad>
2018-04-30 21:15:03 -0600

    code rebuild wip

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,7 +1,13 @@
+#Launch all resources in under dev
+
 module "crm_app_asg_dev" {
   source = "crm_app_asg_dev/"
 }
 
 module "crm_elb_dev" {
   source = "crm_elb_dev/"
+}
+
+module "crm_batch_asg_dev" {
+  source = "crm_batch_asg_dev/"
 }
