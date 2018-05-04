85b8e943e1112b9d730f015b8b31031e87d0fb1d
Chris Mason <chris.mason@cmason-4839.local>
2018-05-04 07:52:14 -0600

    changes

--- environments/test/crm_app_asg_dev/main.tf
+++ environments/test/crm_app_asg_dev/main.tf
@@ -10,8 +10,8 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_region_validation      = true
   skip_credentials_validation = true
-  skip_requesting_account_id  = true
-}*/
+  skip_requesting_account_id  = true*/
+}
 
 ##############################################################
 # Data sources to get VPC, subnets and security group details
